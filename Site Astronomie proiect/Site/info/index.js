63557dea73f14a49ff0608ebd22fab4e8094ab06
rad234 <69139818+rad234@users.noreply.github.com>
2023-06-07 12:32:01 +0300

    Add files via upload

---
+++ Site Astronomie proiect/Site/info/index.js
@@ -0,0 +1,14 @@
+function marcheazaUnu() 
+{
+    document.getElementById("status-unu").innerText="Articol citit în sesiunea aceasta"
+}
+
+function marcheazaDoi() 
+{
+    document.getElementById("status-doi").innerText="Articol citit în sesiunea aceasta"
+}
+
+function marcheazaTrei()
+{
+    document.getElementById("status-trei").innerText="Articol citit în sesiunea aceasta"
+}
